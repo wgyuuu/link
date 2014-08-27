e327ac4f2be75c8edbfa04c481d5747871fea352
fanngyuan <fanngyuan@gmail.com>
2014-08-27 18:05:54 +0800

    add mock session

--- session.go
+++ session.go
@@ -6,6 +6,19 @@ import (
 	"sync/atomic"
 )
 
+type SessionAble interface {
+	Start()
+	Id() uint64
+	Conn() net.Conn
+	IsClosed() bool
+
+	SyncSendPacket(packet []byte) error
+
+	Send(message Message) error
+
+	SendPacket(packet []byte) error
+}
+
 // Session.
 type Session struct {
 	id     uint64
@@ -46,6 +59,42 @@ func NewSession(id uint64, conn net.Conn, protocol PacketProtocol, sendChanSize 
 		closeWait:      new(sync.WaitGroup),
 		closeFlag:      -1,
 	}
+}
+
+type MockSession struct {
+	id     uint64
+	sendPacketChan chan []byte
+}
+
+func NewMockSession()*MockSession{
+	return &MockSession{}
+}
+
+func (Session *MockSession) Start(){
+}
+
+func (Session *MockSession) Id() uint64{
+	return Session.id
+}
+
+func (Session *MockSession)	Conn() net.Conn{
+	return nil
+}
+
+func (Session *MockSession)	IsClosed() bool{
+	return false
+}
+
+func (Session *MockSession)	SyncSendPacket(packet []byte) error{
+	return nil
+}
+
+func (Session *MockSession)	Send(message Message) error{
+	return nil
+}
+
+func (Session *MockSession)	SendPacket(packet []byte) error{
+	return nil
 }
 
 // Start the session's read write goroutines.
