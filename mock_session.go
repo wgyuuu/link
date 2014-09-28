8de8eee41c3b7d405315df4e7c2401cc4af0e01e
fanngyuan <fanngyuan@gmail.com>
2014-09-28 11:29:35 +0800

    fix bug

--- mock_session.go
+++ mock_session.go
@@ -1,37 +1,41 @@
 package link
 
+import (
+	"net"
+)
+
 type MockSession struct {
-	id     uint64
+	id             uint64
 	sendPacketChan chan []byte
 }
 
-func NewMockSession()*MockSession{
+func NewMockSession() *MockSession {
 	return &MockSession{}
 }
 
-func (Session *MockSession) Start(){
+func (Session *MockSession) Start() {
 }
 
-func (Session *MockSession) Id() uint64{
+func (Session *MockSession) Id() uint64 {
 	return Session.id
 }
 
-func (Session *MockSession)	Conn() net.Conn{
+func (Session *MockSession) Conn() net.Conn {
 	return nil
 }
 
-func (Session *MockSession)	IsClosed() bool{
+func (Session *MockSession) IsClosed() bool {
 	return false
 }
 
-func (Session *MockSession)	SyncSendPacket(packet []byte) error{
+func (Session *MockSession) SyncSendPacket(packet []byte) error {
 	return nil
 }
 
-func (Session *MockSession)	Send(message Message) error{
+func (Session *MockSession) Send(message Message) error {
 	return nil
 }
 
-func (Session *MockSession)	SendPacket(packet []byte) error{
+func (Session *MockSession) SendPacket(packet []byte) error {
 	return nil
 }
