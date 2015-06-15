b4d00fba512d2c885d5faefe5bfe59f6a04e5eb7
jixiuf <jixiuf@gmail.com>
2015-06-15 15:41:03 +0800

    buffermgr 粒度更细化

--- buffer/buffer_mgr.go
+++ buffer/buffer_mgr.go
@@ -5,37 +5,67 @@ import (
 )
 
 type BufferPoolMgr struct {
-	defaultBufferSize int // if size<defaultBufferSize ,get from defaultPool ,if not get from biggerPool
-	defaultPool       sync.Pool
-	biggerPool        sync.Pool
+	defaultBufferSize int       // if size<defaultBufferSize ,get from defaultPool ,if not get from size5Pool
+	defaultPool       sync.Pool // cache cap(data)<=defaultBufferSize
+	size5Pool         sync.Pool // cache defaultBufferSize<cap(data)<=defaultBufferSize*5 and
+	size10Pool        sync.Pool // cache 10*defaultBufferSize<cap(data)
+	anyBiggerPool     sync.Pool // 10*defaultBufferSize>cap(data)
 }
 
 func NewBufferPoolMgr(defaultBufferSize int) *BufferPoolMgr {
 	return &BufferPoolMgr{
 		defaultBufferSize: defaultBufferSize,
 		defaultPool:       sync.Pool{},
-		biggerPool:        sync.Pool{},
+		size5Pool:         sync.Pool{},
+		anyBiggerPool:     sync.Pool{},
 	}
 }
 
 func (pool *BufferPoolMgr) Put(data []byte) {
-	if data == nil || cap(data) == 0 {
+	var capSize int
+	if data == nil {
+		return
+	}
+	capSize = cap(data)
+	if capSize == 0 {
 		return
 	}
 
-	if cap(data) > pool.defaultBufferSize {
-		pool.biggerPool.Put(data)
-	} else {
+	if capSize <= pool.defaultBufferSize {
 		pool.defaultPool.Put(data)
+	} else if capSize <= 5*pool.defaultBufferSize {
+		pool.size5Pool.Put(data)
+	} else if capSize <= 10*pool.defaultBufferSize {
+		pool.size10Pool.Put(data)
+	} else {
+		pool.anyBiggerPool.Put(data)
 	}
 
 }
 
-func (pool *BufferPoolMgr) getBigger(size int) (data []byte) {
-	bufferObj := pool.biggerPool.Get()
+func (pool *BufferPoolMgr) Get(size int) (data []byte) {
+	capSize := size
+	if size < pool.defaultBufferSize {
+		capSize = pool.defaultBufferSize
+	}
+	if capSize <= pool.defaultBufferSize {
+		return pool.getDefault(size)
+	} else if capSize <= 5*pool.defaultBufferSize {
+		return pool.getFromSize5Pool(size)
+	} else if capSize <= 10*pool.defaultBufferSize {
+		return pool.getFromSize10Pool(size)
+	} else {
+		return pool.getFromAnyBiggerPool(size)
+	}
+}
+
+func (pool *BufferPoolMgr) getByCreate(size int) (data []byte) {
+	return make([]byte, size, size)
+}
+func (pool *BufferPoolMgr) getFromAnyBiggerPool(size int) (data []byte) {
+	bufferObj := pool.anyBiggerPool.Get()
 	if bufferObj == nil {
-		data = make([]byte, size, size)
-		return
+		return pool.getByCreate(size)
 	}
 	bufferData := bufferObj.([]byte)
 	if cap(bufferData) >= size {
@@ -44,21 +74,39 @@ func (pool *BufferPoolMgr) getBigger(size int) (data []byte) {
 	}
 	pool.Put(bufferData) // put it back ,because it is not big enough
 
-	data = make([]byte, size, size)
-	return
+	return pool.getByCreate(size)
 
 }
-func (pool *BufferPoolMgr) Get(size int) (data []byte) {
-	capSize := size
-	if size < pool.defaultBufferSize {
-		capSize = pool.defaultBufferSize
+func (pool *BufferPoolMgr) getFromSize10Pool(size int) (data []byte) {
+	bufferObj := pool.size10Pool.Get()
+	if bufferObj == nil {
+		return pool.getFromAnyBiggerPool(size)
 	}
-	if capSize > pool.defaultBufferSize {
-		return pool.getBigger(size)
+	bufferData := bufferObj.([]byte)
+	if cap(bufferData) >= size {
+		bufferData = bufferData[0:size]
+		return bufferData
 	}
-	return pool.getDefault(size)
+	pool.Put(bufferData) // put it back ,because it is not big enough
+
+	return pool.getFromAnyBiggerPool(size)
 
 }
+func (pool *BufferPoolMgr) getFromSize5Pool(size int) (data []byte) {
+	bufferObj := pool.size5Pool.Get()
+	if bufferObj == nil {
+		return pool.getFromSize10Pool(size)
+	}
+	bufferData := bufferObj.([]byte)
+	if cap(bufferData) >= size {
+		bufferData = bufferData[0:size]
+		return bufferData
+	}
+	pool.Put(bufferData) // put it back ,because it is not big enough
+
+	return pool.getFromSize10Pool(size)
+}
+
 func (pool *BufferPoolMgr) getDefault(size int) (data []byte) {
 	bufferObj := pool.defaultPool.Get()
 	if bufferObj == nil {
--- buffer/buffer_mgr_test.go
+++ buffer/buffer_mgr_test.go
@@ -23,6 +23,17 @@ func TestBuffer(t *testing.T) {
 	assert.Equal(t, len(data), 5)
 	assert.Equal(t, cap(data), 5)
 
+	data = pool.Get(4*5 + 1)
+	assert.Equal(t, len(data), 4*5+1)
+	assert.True(t, cap(data) >= 4*5+1)
+
+	data = pool.Get(4*5 + 2)
+	assert.Equal(t, len(data), 4*5+2)
+	assert.True(t, cap(data) >= 4*5+2)
 	pool.Put(data)
+
+	data = pool.Get(4*5 + 1)
+	assert.Equal(t, len(data), 4*5+1)
+	assert.True(t, cap(data) >= 4*5+1)
 
 }
